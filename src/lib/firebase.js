ac08c76c8432b12cd0b57e0b08104c2132af6052
Oestomil <69206402+Oestomil@users.noreply.github.com>
2025-10-12 04:39:48 +0300

    ç

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxx2nELSfto51N-_u0tNz49hq8lgM-foA",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "emare-4aab7.firebaseapp.com",
   projectId: "emare-4aab7",
   storageBucket: "emare-4aab7.firebasestorage.app",
